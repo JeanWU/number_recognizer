b1eb749c484d22650410464ed081354459a17049
JeanWU <jean1015jean1015@gmail.com>
2016-03-23 16:26:45 +0800

    0324update

--- machines/machine_loader.py
+++ machines/machine_loader.py
@@ -3,7 +3,7 @@ from sklearn.externals import joblib
 
 
 class MachineLoader():
-    MACHINE_FILE = "machineKN.pkl"
+    MACHINE_FILE = "machine_0323.pkl"
     DATA_FOLDER = os.path.join(os.path.dirname(__file__), "../data/")
     FEEDBACK_FILE_FORMAT = "{0}_feedback.txt"
 
